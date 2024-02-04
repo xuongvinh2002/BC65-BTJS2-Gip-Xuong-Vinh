feb0655a5e466857e0895757a0b23a5edc71c70c
xuongvinh2002 <xuongvinh2001@gmail.com>
2024-02-05 03:41:22 +0700

    add file BTJS2

--- js/index.js
+++ js/index.js
@@ -3,49 +3,51 @@
  lương 1 ngày 100000
  số ngày làm n
  B2 xác định các bước xử lý 
- Tiền Lương = số ngày làm n * 100000
+ + Dom tới html để lấy giá trị của các thẻ input
+ + Tiền Lương = số ngày làm n * 100000
  B3 Ra kết quả */
+ function handleMoney(){
+ var luong1Ngay= +document.getElementById("luong1Ngay").value;
+ var soNgayLam = +document.getElementById("soNgayLam").value;
+ var result = luong1Ngay*soNgayLam;
+ document.getElementById("result").innerHTML= result;
+ document.getElementById("result").classList.remove("d-none");
 
- var luong1Ngay=100000;
- var soNgayLam = 23;
- var tienLuong;
- 
- tienLuong= luong1Ngay*soNgayLam;
- console.log("tienLuong:",tienLuong);
- 
+ }
  // bài 2
  /* B1 xác định dự liệu đầu vào
  nhập 5 số thực a,b,c,d,e
  biến chưa giá trị trung bình
   B2 xác định các bước xử lý 
- giá trị trung bình= (a+b+c+d+e)/5
+  + Dom tới html để lấy giá trị của các thẻ input
+  + giá trị trung bình= (a+b+c+d+e)/5
  B3 in ra kết quả*/
- var a = 1;
- var b = 2;
- var c = 3;
- var d = 4;
- var e = 5;
- var giaTriTrungBinh;
- 
- giaTriTrungBinh =(a+b+c+d+e)/5;
- console.log("giaTriTrungBinh:",giaTriTrungBinh);
- 
+ function handleAverageValue() {
+ var a = +document.getElementById("nhapSoThuNhat").value;
+ var b = +document.getElementById("nhapSoThuHai").value;
+ var c = +document.getElementById("nhapSoThuBa").value;
+ var d = +document.getElementById("nhapSoThuTu").value;
+ var e = +document.getElementById("nhapSoThuNam").value;
+ var result1 =(a+b+c+d+e)/5;
+ document.getElementById("result1").innerHTML= result1;
+ document.getElementById("result1").classList.remove("d-none");
+ }
  // Bài 3
  /* B1 xác định dự liệu đầu vào
  Giá USD = 23500VND
  nhập giá trị n USD
   B2 xác định các bước xử lý 
-  Giá trị VND =  nUSD *23500
+  + Dom tới html để lấy giá trị của các thẻ input
+  + Giá trị VND =  nUSD *23500
   B3 xuất ra kết quả*/
-     
- var giaTriUSD =23500;
- var nhapUSD = 5;
- var giaTriVND;
- 
- giaTriVND = giaTriUSD * nhapUSD;
- console.log("giaTriVND;",giaTriVND);
- 
- 
+  function handleMoneyVND(){  
+ var giaTriUSD = +document.getElementById("giaTriUSD").value;
+ var nhapUSD = +document.getElementById("nhapUSD").value;
+ var result2 = giaTriUSD * nhapUSD;
+ document.getElementById("result2").innerHTML= result2;
+ document.getElementById("result2").classList.remove("d-none");
+  
+  }
  
  // Bài 4
  /* B1 xác định dự liệu đầu vào
@@ -55,35 +57,46 @@
   +biến chứa diện tích
   
   B2 xác định các bước xử lý 
-  dien tich= dai * rong
-  chu vi = (dai + rong)*2
+  + Dom tới html để lấy giá trị của các thẻ input
+  + dien tich= dai * rong
+  + chu vi = (dai + rong)*2
   B3 Ra kết quả*/
-  var chieuDai = 4;
-  var chieuRong =3;
-  var dienTich;
-  var chuVi;
- 
-  dienTich= chieuDai * chieuRong;
-  console.log("dienTich:",dienTich);
-  chuVi = (chieuDai + chieuRong)*2;
-  console.log("chuVi:",chuVi);
- 
+  function handleChuVi(){ 
+  var chieuDai = +document.getElementById("chieuDai").value;
+  var chieuRong = +document.getElementById("chieuRong").value;
+  var result3 = chieuDai * chieuRong;
+  var result3 = (chieuDai + chieuRong)*2;
+  document.getElementById("result3").innerHTML= result3;
+  document.getElementById("result3").classList.remove("d-none");
+  }
+  function handleDienTich(){ 
+    var chieuDai = +document.getElementById("chieuDai").value;
+    var chieuRong = +document.getElementById("chieuRong").value;
+    var result3 = chieuDai * chieuRong;
+
+    document.getElementById("result3").innerHTML= result3;
+    document.getElementById("result3").classList.remove("d-none");
+    }
+
+  
  
  // bài 5
  /* B1 xác định dự liệu đầu vào
   số nguyên n
- biến chứa tổng và chứa 3 ký số
+  kết quả
  
   B2 xác định các bước xử lý 
- 
-  Xác định ký số hàng chục : Math.floor(n/10) 
-  Xác định ký số hàng đơn vị :n%10
+  + Dom tới html để lấy giá trị của các thẻ input
+  + Xác định ký số hàng chục : Math.floor(n/10) 
+  + Xác định ký số hàng đơn vị :n%10
  
   B3: kết quả */
- 
- var n =53;
- var sum;
+ function handleTotal(){ 
+ var n = +document.getElementById("nhap1So").value;
  var hangChuc =  Math.floor(n/10);
  var hangDonVi = n%10;
- sum= hangChuc + hangDonVi;
- console.log("sum:",sum);
+ var result4 = hangChuc + hangDonVi;;
+
+ document.getElementById("result4").innerHTML= result4;
+ document.getElementById("result4").classList.remove("d-none");
+ }
